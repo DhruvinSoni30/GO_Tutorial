4e1759d8ab1112dbc585a90f998e0a5ce466764b
Dhruvin Soni2 <DhruvinSoni30>
2023-07-07 17:37:18 +0530

    Updating code

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
@@ -9,16 +10,62 @@ func main() {
 	const conferenceTickets = 50
 	// unit will make sure the remainingTickets will not be negetive
 	var remainingTickets uint = 50
+	var bookings = []string{}
 
 	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
 	fmt.Printf("We have total of %v and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
 
-	var userName string
-	var userTickets int
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	userName = "Dhruvin"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
 
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
+
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
+
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+		if isValidName && isValidEmail && isValidTicketNumber {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
+
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("These firtsnames of bookings are: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				fmt.Print("Our conference is booked out, Come back next year!\n")
+				break
+			}
+		} else {
+			if !isValidName {
+				fmt.Printf("First name or Last name you entered is too short, try again.\n")
+			}
+			if !isValidEmail {
+				fmt.Printf("Email address you entered is invalid")
+			}
+			if !isValidTicketNumber {
+				fmt.Printf("Number of tickets you entered is invalid")
+			}
+		}
+	}
 }
